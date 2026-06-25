b2a0cdfecc330ae266ba5e310d65b7e61b2e98cc
tecnodeveloper <recluzedev@gmail.com>
2026-06-25 08:56:41 +0500

    feat(capstone): add SQLite dependency and draft dashboard input logicInclude SQLiteCpp/SQLiteCpp.h header in main.cpp.Add user input handling and basic validation loop for the main menu.Add staged.log to .gitignore.Track a new binary file testDB.Rename ER-Diagram documentation markdown to replace commas with hyphens.

--- 1st_Round(Technical Round)/01-Basic-University-Questions/01-OOP/Capstone Project/main.cpp
+++ 1st_Round(Technical Round)/01-Basic-University-Questions/01-OOP/Capstone Project/main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <SQLiteCpp/SQLiteCpp.h>
+
 using namespace std;
 class Book{
 private:
@@ -25,9 +27,20 @@ int main(){
     for(int i = 1; i < 5;i++){
         cout<<"---------------------------------------------------------------\n";
     }
-
+    int dashboardinput = 0;
     cout<<"Press 1 to 'Login as Admin' \n";
     cout<<"Press 2 to 'Login as User' \n";
     cout<<"Press 3 to 'Exit Program' \n";
+    cin>>dashboardinput;
+    if(dashboardinput == 1 || dashboardinput == 2 || dashboardinput == 3){
+        
+    }else{
+        while (dashboardinput == 1 || dashboardinput == 2 || dashboardinput == 3) {
+               
+        }
+    }
+        
+    
+
 
 }
